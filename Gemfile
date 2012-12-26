6c321ecba9c22bfc6c5f12d94442ec17df98d6ad
pkagrawal <agrawal.pradyumna@gmail.com>
2012-12-26 15:51:11 +0530

    password added to users

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ end
 gem 'jquery-rails'
 gem 'execjs'
 gem 'therubyracer'
+gem 'bootstrap-sass'
+gem 'bcrypt-ruby'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+  before_save { |user| user.email = email.downcase }
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
