678b55dfd3c1fa14ac1adc87dcf80b737a9218d2
pkagrawal <agrawal.pradyumna@gmail.com>
2012-12-26 13:40:45 +0530

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mobileresearch::Application.config.secret_token = 'd9ad26ad94130049a933b7fa74e7cb71d235d2a7ccbd608be350c13b82668367c610508b29d61a820309c47823def37f2033d439e1d1b5665233955647c79847'
